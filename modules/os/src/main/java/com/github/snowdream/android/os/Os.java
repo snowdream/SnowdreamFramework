42a0051b063b6cce3ebe7f456229f0ebae0c2f06
snowdream <yanghui1986527@gmail.com>
2015-11-03 22:52:14 +0800

    Add: os.uptime  and os.EOL

--- modules/os/src/main/java/com/github/snowdream/android/os/Os.java
+++ modules/os/src/main/java/com/github/snowdream/android/os/Os.java
@@ -1,9 +1,30 @@
 package com.github.snowdream.android.os;
 
+import android.os.SystemClock;
+
 /**
  * Created by snowdream on 11/17/13.
  */
 public class Os {
 
 
+    /**
+     * Returns the system uptime in seconds.
+     *
+     * @param isIncludeSleepTime including time spent in sleep.
+     *
+     * @return
+     */
+    public static final long uptime(boolean isIncludeSleepTime){
+        if (isIncludeSleepTime){
+            return SystemClock.elapsedRealtime();
+        }else{
+            return SystemClock.uptimeMillis();
+        }
+    }
+
+    /**
+     *  A constant defining the appropriate End-of-line marker for the operating system.
+     */
+    public static final String EOL = "\n";
 }

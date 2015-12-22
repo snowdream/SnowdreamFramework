9f14370f0912e2f25b252462b72a8bfab4c48b8b
snowdream <yanghui1986527@gmail.com>
2015-12-22 23:14:20 +0800

    Add:apidemo

---
+++ ApiDemos/src/androidTest/java/com/github/snowdream/android/apidemos/HelloWorldLibTest.java
@@ -0,0 +1,23 @@
+package com.github.snowdream.android.apidemos;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by snowdream on 4/8/14.
+ */
+public class HelloWorldLibTest extends AndroidTestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testgetHelloWorld() {
+    }
+
+}
